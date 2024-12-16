74d2919bd69ec11b7402e2d106ecf56317240905
enrique <enriqueadhemar@gmail.com>
2024-12-16 15:33:36 -0400

    feat: bucket public read policy

--- infrastructure/storage.tf
+++ infrastructure/storage.tf
@@ -7,3 +7,24 @@ resource "aws_s3_bucket" "ems_bucket" {
     Environment = "production"
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "bucket_public_access" {
+  bucket = aws_s3_bucket.ems_bucket.id
+}
+
+resource "aws_s3_bucket_policy" "bucket_public_policy" {
+  bucket = aws_s3_bucket.ems_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.ems_bucket.arn}/*"
+      }
+    ]
+  })
+}
